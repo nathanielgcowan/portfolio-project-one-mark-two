37b51431da9b55a9e57ca929b76a6deffa134b13
Nathaniel Cowan <nathaniel.g.cowan@gmail.com>
2025-03-28 18:38:26 -0500

    test

---
+++ src/app/components/AddTask.tsx
@@ -0,0 +1,8 @@
+
+const AddTask = () => {
+    return <div>
+        <button className="btn btn-primary w-full">Add new task</button>
+    </div>;
+};
+
+export default AddTask;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,12 @@
-import Image from "next/image";
+import AddTask from "./components/AddTask";
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      Test
-    </div>
+    <main className="max-w-4xl mx-auto mt-4">
+      <div className="text-center my-5 flex flex-col gap-4">
+        <h1 className="text-2xl font-bold">Todo List App</h1>
+        <AddTask />
+      </div>
+    </main>
   );
 }
